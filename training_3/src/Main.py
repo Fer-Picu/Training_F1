452626476919ad0b9dd7fa2c52271ca394ef8859
Fer-Picu <piaggi.fer@gmail.com>
2013-03-26 16:46:14 -0300

    iniciado el ejercicio 3

---
+++ training_3/src/Main.py
@@ -0,0 +1,38 @@
+'''
+Created on 26/03/2013
+
+@author: fernando
+'''
+import Tkinter
+import tkMessageBox
+i = 0
+
+
+class Modelo_Boton_1():
+    def __init__(self):
+        self.i = 0
+    def accion_boton_1(self):
+        self.i = self.i + 1
+        print self.i
+        
+        
+modelo_boton_1 = Modelo_Boton_1()
+
+if __name__ == '__main__':  
+    top = Tkinter.Tk()
+    boton_1 = Tkinter.Button(top, text ="Hello", command = modelo_boton_1.accion_boton_1)
+    boton_1.pack()
+    top.mainloop()
+
+
+diccionario_letras = {'2':'a', '22':'b', '222':'c',\
+                      '3':'d', '33':'e', '333':'f',\
+                      '4':'g', '44':'h', '444':'i',\
+                      '5':'j', '55':'k', '555':'l',\
+                      '6':'m', '66':'n', '666':'o',\
+                      '7':'p', '77':'q', '777':'r', '7777':'s',\
+                      '8':'t', '88':'u', '888':'v',\
+                      '9':'w', '99':'x', '999':'y', '9999':'z'\
+                      }
+
+
